8e2e6e8d035e550241c2a405a820d785d65d713a
AngelMarciales <angel.marciales@uptc.edu.co>
2023-05-29 17:47:06 -0500

    Creacion de rutas y algunos componentes prueba

---
+++ src/components/TaskDetail.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>TAsk Detail</h1>
+</template>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>HOLA</h1>
+    <form>
+        <input type="text" placeholder="Ingrese valor">
+        <button>Save</button>
+    </form>
+</template>
